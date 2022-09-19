f108fde4d790a7b67bf693eb5a1945ec5fd473b6
nicoelias997 <nicoelias997@gmail.com>
2022-09-19 15:57:26 +0200

    Agrego unos botones para mostrar la secuencia de los ejercicios 1,2,3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Ejercicio1 from "./components/ejercicio1";
 function App() {
   const [clave, setClave] = React.useState("");
   const [ejercicio1, setEjercicio1] = React.useState(false);
+  const [ejercicio2, setEjercicio2] = React.useState(false);
 
-  const mostrarClave = () => {
-    setClave("(changed from state)!");
+  const mostrarEjercicio1 = () => {
+    setClave("");
+    setEjercicio1(false);
+    setEjercicio2(false);
   };
   const mostrarEjercicio2 = () => {
     setEjercicio1(true);
+    setEjercicio2(true);
+  };
+  const mostrarClave = () => {
+    setClave("(changed from state)!");
+    setEjercicio2(false);
   };
 
   return (
@@ -17,7 +25,8 @@ function App() {
       <h1>Ejercicios 1 al 3 - TP8 - React JS</h1>
       <hr />
 
-      {ejercicio1 === false ? ( //condicion verdadera
+      {ejercicio1 === false ? (
+        //condicion verdadera(si estamos en el ej 1)
         <div>
           <Ejercicio1 mensaje="world"></Ejercicio1>
 
@@ -30,16 +39,27 @@ function App() {
           </button>
         </div>
       ) : (
-        //condicion falsa
+        //condicion falsa(si estamos en el ej 2)
+
         <div>
           <Ejercicio1 mensaje="My friend" clave={clave}></Ejercicio1>
-          <button
-            type="button"
-            onClick={() => mostrarClave()}
-            className="btn btn-dark"
-          >
-            Mostrar ejercicio 3
-          </button>
+          {ejercicio2 === true ? (
+            <button
+              type="button"
+              onClick={() => mostrarClave()}
+              className="btn btn-dark"
+            >
+              Mostrar ejercicio 3
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => mostrarEjercicio1()}
+              className="btn btn-warning"
+            >
+              Volver al ejercicio 1
+            </button>
+          )}
         </div>
       )}
     </div>
