8bb3605a8e068638af6e35a5b3e1b3e9444a51ef
nicoelias997 <nicoelias997@gmail.com>
2022-09-19 13:42:01 +0200

    agrego cdn de bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ function App() {
   return (
     <div className="container mt-5">
       <h1>Ejercicios 1 al 3 - TP8 - React JS</h1>
+    <div className="d-flex justify-content-between">
+      <button 
+      type="button"
+
+      >Ejercicio 1
+      </button>
+      <button 
+      type="button"
+
+      >Ejercicio 2
+      </button>
+      <button 
+      type="button"
+
+      >Ejercicio 3
+      </button>
+    </div>
     </div>
   );
 }
