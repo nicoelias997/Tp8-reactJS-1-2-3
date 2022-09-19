84efd5bb000f0f07d9e106f11f9c3cb401161b43
nicoelias997 <nicoelias997@gmail.com>
2022-09-19 15:26:54 +0200

    Agrego botones para que se visualicen los siguientes ejercicios

--- src/App.js
+++ src/App.js
@@ -5,21 +5,43 @@ import Ejercicio1 from "./components/ejercicio1";
 function App() {
 
   const [clave, setClave] = React.useState("")
+  const [ejercicio1, setEjercicio1] = React.useState(false)
 
   const mostrarClave = () => {
     setClave("(changed from state)!")
+  }
+  const mostrarEjercicio2 = () => {
+    setEjercicio1(true)
   }
 
   return (
     <div className="container mt-5">
       <h1>Ejercicios 1 al 3 - TP8 - React JS</h1>
-    <Ejercicio1 mensaje="world"></Ejercicio1>
-    <Ejercicio1 mensaje="My friend" clave={clave}></Ejercicio1>
-    <button 
+      <hr />
+    
+        {
+          ejercicio1 === false ? //condicion verdadera
+          <div>
+        <Ejercicio1 mensaje="world"></Ejercicio1>
+
+          <button 
+      type="button"
+      className="btn btn-danger"
+      onClick={() => mostrarEjercicio2()}
+      >
+      Mostrar ejercicio 2
+      </button> 
+      </div>
+      : //condicion falsa
+      <div>
+      <Ejercicio1 mensaje="My friend" clave={clave}></Ejercicio1>
+      <button 
     type="button"
     onClick={() => mostrarClave()}
-    className="btn btn-dark">Mostrar clave
+    className="btn btn-dark">Mostrar ejercicio 3
     </button>
+      </div>
+      }     
     </div>
   );
 }
