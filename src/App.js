0c56c430b7d2f24f487c167a9e36ead7eef9446f
nicoelias997 <nicoelias997@gmail.com>
2022-09-19 14:09:22 +0200

    Agrego props al ejercico 1 para mostrar el ej1, y 2 a la vez, luego agregue un boton donde agregaria otro props.clave pero desde el state y mediante una funcion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
+import Ejercicio1 from "./components/ejercicio1";
+
 
 function App() {
+
+  const [clave, setClave] = React.useState("")
+
+  const mostrarClave = () => {
+    setClave("(changed from state)!")
+  }
+
   return (
     <div className="container mt-5">
       <h1>Ejercicios 1 al 3 - TP8 - React JS</h1>
-    <div className="d-flex justify-content-between">
-      <button 
-      type="button"
-
-      >Ejercicio 1
-      </button>
-      <button 
-      type="button"
-
-      >Ejercicio 2
-      </button>
-      <button 
-      type="button"
-
-      >Ejercicio 3
-      </button>
-    </div>
+    <Ejercicio1 mensaje="world"></Ejercicio1>
+    <Ejercicio1 mensaje="My friend" clave={clave}></Ejercicio1>
+    <button 
+    type="button"
+    onClick={() => mostrarClave()}
+    className="btn btn-dark">Mostrar clave
+    </button>
     </div>
   );
 }
---
+++ src/components/ejercicio1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ejercicio1 = (props) => {
+
+
+  return (
+   
+    <div>
+        <h1>Hello {props.mensaje}{props.clave}</h1>
+    </div>
+  )
+}
+
+export default ejercicio1
